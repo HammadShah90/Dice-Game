cee1380a53ad352f41451767c55b9d5d8553ce71
HammadShah <121276577+HammadShah90@users.noreply.github.com>
2023-03-19 16:50:06 +0500

    previous scor add and name add

--- app.js
+++ app.js
@@ -1,84 +1,146 @@
+// --------------Player1 Information----------
+
+var player1Name = prompt("Tell me Player 1 Name..??");
+var player1NameChange = document.querySelector('.player-1');
+
+// console.log(player1NameChange.textContent);
+
+player1NameChange.textContent = player1Name.slice(0, 1).toUpperCase() + player1Name.slice(1).toLowerCase();
+var player1 = document.querySelector('.player1');
+var player1Score = document.querySelector('.player1Score');
+var player1DiceScore = document.querySelector('.player1DiceScore');
+var player1PreviousScore = document.querySelector('.player1PreviousScore');
+
+// console.log(player1NameChange.textContent);
+
+// --------------Player2 Information----------
+
+var player2Name = prompt("Tell me Player 2 Name..??");
+var player2NameChange = document.querySelector('.player-2');
+
+// console.log(player2NameChange.textContent);
+
+player2NameChange.textContent = player2Name.slice(0, 1).toUpperCase() + player2Name.slice(1).toLowerCase();
+var player2 = document.querySelector('.player2');
+var player2Score = document.querySelector('.player2Score');
+var player2DiceScore = document.querySelector('.player2DiceScore');
+var player2PreviousScore = document.querySelector('.player2PreviousScore');
+
+// console.log(player2NameChange.textContent);
+
+// --------------Others Information----------
+
 var newGameBtn = document.querySelector('.newGameBtn');
 var diceImg = document.querySelector('.diceImg');
 var rollDiceBtn = document.querySelector('.rollDiceBtn');
 var holdBtn = document.querySelector('.holdBtn');
-var player1Score = document.querySelector('.player1Score');
-var player2Score = document.querySelector('.player2Score');
-var player1DiceScore = document.querySelector('.player1DiceScore');
-var player2DiceScore = document.querySelector('.player2DiceScore');
-var player1 = document.querySelector('.player1');
-var player2 = document.querySelector('.player2');
 var activePlayer = player1;
-var diceScore = player1DiceScore;
-var playerScore = player1Score;
-var winnerAnnouce = document.querySelector('.winnerAnnouce');
-var overlay = document.querySelector('.overlay')
+
+// console.log(player2NameChange);
+
+// --------------Create Functions----------
+
+function diceChanging() {
+    var diceNum = Math.round(Math.random() * 5) + 1;
+    diceImg.src = `./assets/${diceNum}.png`;
+    if (diceNum == 1) {
+        if (activePlayer == player1) {
+            
+            activePlayer = player2;
+
+            player1PreviousScore.textContent = +player1DiceScore.textContent;
+            
+            player1DiceScore.textContent = 0;
+
+            change2Bg();
+            
+        } else {
+            
+            activePlayer = player1;
+            
+            player2PreviousScore.textContent = +player2DiceScore.textContent;
+            
+            player2DiceScore.textContent = 0;
+
+            change1Bg();
+        }
+        
+        player1.classList.toggle('active');
+        player2.classList.toggle('active');
+        
+    } else {
+        if (activePlayer == player1) {
+            
+            player1DiceScore.textContent = +player1DiceScore.textContent + diceNum
+            
+        } else {
+            
+            player2DiceScore.textContent = +player2DiceScore.textContent + diceNum
+            
+        }
+    }
+};
+
+function newGame() {
+    location.reload();
+}
 
 
-function changeBg() {
+function holdScore() {
+    
+    player1.classList.toggle('active');
+    player2.classList.toggle('active');
+    
+    if (activePlayer == player1) {
+        
+        activePlayer = player2;
+        
+        player1Score.textContent = +player1Score.textContent + Number(player1DiceScore.textContent)
+        
+        player1PreviousScore.textContent = +player1DiceScore.textContent;
+        
+        player1DiceScore.textContent = 0
+
+        change2Bg();
+        
+        if (Number(player1Score.textContent) >= 50) {
+            alert(`${player1Name.toUpperCase()} Winner`);
+            newGame();
+        }
+    } else {
+        
+        activePlayer = player1;
+        
+        player2Score.textContent = +player2Score.textContent + Number(player2DiceScore.textContent)
+        
+        player2PreviousScore.textContent = +player2DiceScore.textContent;
+        
+        player2DiceScore.textContent = 0
+
+        change1Bg();
+
+        if (Number(player2Score.textContent) >= 50) {
+            alert(`${player2Name.toUpperCase()} Winner`);
+            newGame();
+        }
+    }
+}
+
+function change2Bg() {
     activePlayer.style.borderTopLeftRadius = "0px"
     activePlayer.style.borderBottomLeftRadius = "0px"
     activePlayer.style.borderTopRightRadius = "10px"
     activePlayer.style.borderBottomRightRadius = "10px"
 }
 
-function newGame() {
-    location.reload();
+function change1Bg() {
+    activePlayer.style.borderTopLeftRadius = "10px"
+    activePlayer.style.borderBottomLeftRadius = "10px"
+    activePlayer.style.borderTopRightRadius = "0px"
+    activePlayer.style.borderBottomRightRadius = "0px"
 }
 
-function diceChanging() {
-    var diceNum = Math.round(Math.random() * 5) + 1;
-    diceImg.src = `./assets/${diceNum}.png`;
-    if (diceNum == 1) {
-        diceScore.textContent = 0;
-        playerMoving();
-    } else {
-        diceScore.textContent = +diceScore.textContent + diceNum
-    }
-};
-
-function holdScore() {
-    playerScore.textContent = Number(playerScore.textContent) + Number(diceScore.textContent)
-
-    diceScore.textContent = 0
-
-    if (playerScore.textContent >= 50) {
-        if (activePlayer == player1) {
-            alert(`Player 1 Wins`);
-            newGame();
-        } else {
-            alert(`Player 2 Wins`);
-            newGame();
-        }
-    } else {
-        playerMoving();
-    }
-}
-
-function playerMoving() {
-    if (activePlayer == player1) {
-        activePlayer = player2;
-        activePlayer.classList.add('active');
-        player1.classList.remove('active');
-        changeBg();
-    } else if (activePlayer == player2) {
-        activePlayer = player1;
-        activePlayer.classList.add('active');
-        player2.classList.remove('active');
-
-    }
-    playerActivation();
-}
-
-function playerActivation() {
-    if (activePlayer == player1) {
-        diceScore = player1DiceScore;
-        playerScore = player1Score;
-    } else if (activePlayer == player2) {
-        diceScore = player2DiceScore;
-        playerScore = player2Score;
-    }
-}
+// --------------addEvantlistener to click buttons----------
 
 newGameBtn.addEventListener('click', newGame);
 rollDiceBtn.addEventListener('click', diceChanging);
